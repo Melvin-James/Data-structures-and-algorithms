ec9171e4daff505ab17f8d7731324e1ffbfe6859
Melvin-James <melvinjamesk2003@gmail.com>
2025-10-26 16:01:25 +0530

    Add circular queue implementation

--- queue/circularQueueWithMaxLength.js
+++ queue/circularQueueWithMaxLength.js
@@ -7,31 +7,31 @@ class circularQueue{
     }
     enqueue(value){
         if((this.rear+1)%this.size === this.front){
-            console.log('Queue is Full');
+            console.log("Queue is full, can't perform enqueue operation");
             return;
         }
         if(this.front === -1){
             this.front = 0;
-            this.rear =0;
+            this.rear = 0;
         }else{
-            this.rear=(this.rear+1)%this.size
+            this.rear = (this.rear+1)%this.size;
         }
         this.queue[this.rear]=value;
-        console.log(`Inserted ${value}`);
+        console.log(`${value} inserted`);
     }
     dequeue(){
-        if(this.front===-1){
-            console.log('Queue is empty');
+        if(this.front === -1){
+            console.log("Queue is empty, can't perform dequeue operation");
             return;
         }
         const removed = this.queue[this.front];
-        if(this.front===this.rear){
-            this.front=-1;
-            this.rear=-1;
+        if(this.front === this.rear){
+            this.front = -1;
+            this.rear = -1;
         }else{
             this.front = (this.front+1)%this.size;
         }
-        console.log(`removed ${removed}`);
+        console.log(`${removed} removed`);
         return removed;
     }
     display(){
@@ -46,16 +46,25 @@ class circularQueue{
     }
 }
 const cq = new circularQueue(5);
-cq.enqueue(10);
 cq.enqueue(20);
 cq.enqueue(30);
 cq.enqueue(40);
 cq.enqueue(50);
-cq.display();
-cq.dequeue();
-cq.dequeue();
-cq.display();
 cq.enqueue(60);
 cq.enqueue(70);
-cq.enqueue(80);
 cq.display();
+cq.dequeue();
+cq.dequeue();
+cq.dequeue();
+cq.display();
+cq.enqueue(100);
+cq.enqueue(200);
+cq.enqueue(300);
+cq.enqueue(400);
+cq.display();
+cq.dequeue();
+cq.dequeue();
+cq.dequeue();
+cq.dequeue();
+cq.dequeue();
+cq.dequeue();
