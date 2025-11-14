5bbc393e48fac167516dc77a50e6d4cf7c3d411b
Melvin-James <melvinjamesk2003@gmail.com>
2025-11-14 20:08:47 +0530

    Added : trie implementation

---
+++ trie/trieImplementation.js
@@ -0,0 +1,52 @@
+class Node{
+    constructor(){
+        this.children = {};
+        this.isWordEnd = false;
+    }
+}
+class Trie{
+    constructor(){
+        this.root = new Node();
+    }
+    insert(word){
+        let curr = this.root;
+        for(let i=0;i<word.length;i++){
+            let charToInsert = word[i];
+            if(!(charToInsert in curr.children)){
+                curr.children[charToInsert] = new Node();
+            }
+            curr = curr.children[charToInsert];
+        }
+        curr.isWordEnd = true;
+    }
+    contains(word){
+        let curr = this.root;
+        for(let i=0;i<word.length;i++){
+            let charToFind = word[i];
+            if(!(charToFind in curr.children)){
+                return false;
+            }
+            curr = curr.children[charToFind];
+        }
+        return curr.isWordEnd;
+    }
+    startsWithPrefix(prefix){
+        let curr = this.root;
+        for(let i=0;i<prefix.length;i++){
+            let charToFind = prefix[i];
+            if(!(charToFind in curr.children)){
+                return false;
+            }
+            curr = curr.children[charToFind];
+        }
+        return true;
+    }
+}
+
+const trie = new Trie();
+trie.insert('ball');
+trie.insert('bat');
+trie.insert('doll');
+console.log(trie.contains('bat'));
+console.log(trie.contains('bomb'));
+console.log(trie.startsWithPrefix('d'));
