40b65db8ecf7ba7ed068a9574df8509b92f128e7
Melvin-James <melvinjamesk2003@gmail.com>
2025-11-15 13:27:25 +0530

    Added: graph implementation

---
+++ graph/graphImplementation.js
@@ -0,0 +1,67 @@
+class Graph{
+    constructor(){
+        this.adjacencyList = { };
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]){
+            this.adjacencyList[vertex] = new Set();
+        }
+    }
+    addEdge(vertex1,vertex2){
+        if(!this.adjacencyList[vertex1]){
+            this.addVertex(vertex1);
+        }
+        if(!this.adjacencyList[vertex2]){
+            this.addVertex(vertex2);
+        }
+        this.adjacencyList[vertex1].add(vertex2);
+        this.adjacencyList[vertex2].add(vertex1);
+    }
+    display(){
+        for(let vertex in this.adjacencyList){
+            console.log(vertex + '->' +[...this.adjacencyList[vertex]])
+        }
+    }
+    hasEdge(vertex1,vertex2){
+        return (
+            this.adjacencyList[vertex1].has(vertex2) &&
+            this.adjacencyList[vertex2].has(vertex1)
+        );
+    }
+    removeEdge(vertex1,vertex2){
+        this.adjacencyList[vertex1].delete(vertex2);
+        this.adjacencyList[vertex2].delete(vertex1);
+    }
+    removeVertex(vertex){
+        if(!this.adjacencyList[vertex]){
+            return;
+        }
+        for(let adjacentVertex of this.adjacencyList[vertex]){
+            this.removeEdge(vertex,adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
+    BFS(start){
+        const visited = new Set();
+        const queue = [start];
+        visited.add(start);
+        while(queue.length>0){
+            const current = queue.shift();
+            console.log(current);
+            for(let neighbor of this.adjacencyList[current]){
+                if(!visited.has(neighbor)){
+                    visited.add(neighbor);
+                    queue.push(neighbor);
+                }
+            }
+        }
+    }
+}
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addEdge('A','B');
+graph.addEdge('B','C');
+graph.display();
+graph.BFS('B');
