67a5d54d014eaaf42f3b461f5a9af6440642d4ff
Melvin-James <melvinjamesk2003@gmail.com>
2025-11-27 12:11:44 +0530

    Edited : edit in .\countTotalNodesBT.js

--- tree/countTotalNodesBT.js
+++ tree/countTotalNodesBT.js
@@ -1,12 +1,11 @@
-class Node {
+class Node{
     constructor(value){
         this.value = value;
         this.left = null;
         this.right = null;
     }
 }
-
-class binaryTree {
+class binaryTree{
     constructor(){
         this.root = null;
     }
@@ -14,38 +13,40 @@ class binaryTree {
         let node = new Node(value);
         if(!this.root){
             this.root = node;
-        }else{
-            let queue = [this.root];
-            while(queue.length){
-                let currentNode = queue.shift();
-                if(!currentNode.left){
-                    currentNode.left = node;
-                    return;
-                }else if(!currentNode.right){
-                    currentNode.right = node;
-                    return;
-                }
-                queue.push(currentNode.left);
-                queue.push(currentNode.right);
+            return;
+        }
+        let queue = [this.root];
+        while(queue.length){
+            let current = queue.shift();
+            if(!current.left){
+                current.left = node;
+                return;
             }
+            if(!current.right){
+                current.right = node;
+                return;
+            }
+            queue.push(current.left);
+            queue.push(current.right);
         }
     }
     countNodes(){
-        let count = 0;
+        if(!this.root) return;
         let queue = [this.root];
+        let count = 0;
         while(queue.length){
-            let currentNode = queue.shift();
-            count++;
-            if(currentNode.left) queue.push(currentNode.left);
-            if(currentNode.right) queue.push(currentNode.right);
+            let current = queue.shift();
+            if(current) count++;
+            if(current.left) queue.push(current.left);
+            if(current.right) queue.push(current.right);
         }
         return count;
     }
 }
 const bt = new binaryTree();
-bt.insert(10);
-bt.insert(5);
-bt.insert(15);
-bt.insert(3);
-bt.insert(7);
-console.log('Number of nodes in tree:',bt.countNodes());
+bt.insert('A');
+bt.insert('B');
+bt.insert('C');
+bt.insert('D');
+bt.insert('E');
+console.log('Number of nodes:',bt.countNodes());
