3a3f1d612be78332f49890b25511a0e62e33f1e7
Melvin James K <melvinjamesk2003@gmail.com>
2026-07-29 16:29:08 +0530

    Added: find closest value in bst for a target value

---
+++ tree/binarySearchTree/closestValue.js
@@ -0,0 +1,78 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+
+    insert(value){
+        let node = new Node(value);
+        if(!this.root){
+            this.root = node;
+            return;
+        }
+        let current = this.root;
+        while(true){
+            if(value < current.value){
+                if(!current.left){
+                    current.left = node;
+                    return;
+                }
+                current = current.left;
+            }else{
+                if(!current.right){
+                    current.right = node;
+                    return;
+                }
+                current = current.right;
+            }
+        }
+    }
+
+    closestValue(target){
+        if(!this.root) return;
+        let current = this.root;
+        let closest = this.root.value;
+        while(current){
+            if(Math.abs(target - current.value) < Math.abs(target - closest)){
+                closest = current.value;
+            }
+            if(target < current.value){
+                current = current.left;
+            }else if(target > current.value){
+                current = current.right;
+            }else{
+                break;
+            }
+        }
+        return closest;
+    }
+
+    levelOrder(){
+        if(!this.root) return;
+        let queue = [this.root];
+        while(queue.length){
+            let current = queue.shift();
+            console.log(current.value);
+            if(current.left) queue.push(current.left);
+            if(current.right) queue.push(current.right);
+        }
+    }
+
+}
+
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(3);
+bst.insert(5);
+bst.insert(8);
+bst.insert(12);
+bst.levelOrder();
+console.log('Closest value:',bst.closestValue(13));
+
